ef0899bfdb450893fda81a4593991aca6882652d
whuiyiu <whuiyiu@gmail.com>
2014-08-04 20:07:49 +0800

    Quantmod Lecture 2

--- quantmodStudy/quantmod2.r
+++ quantmodStudy/quantmod2.r
@@ -20,4 +20,40 @@ AAPL.Cl<-Delt(Cl(get("AAPL",envir=myenv)),type=("arithmetic"))
 AAPL.Cl[which(abs(AAPL.Cl)>0.02),]
 plot(AAPL.Cl)
 
+####计算收益率
+getSymbols("AAPL",src="yahoo",from="2013-10-01",to="2013-10-30")
+##daily
+periodReturn(AAPL,period="daily")
+periodReturn(AAPL,period="daily",subset="2013-10-01::2013-10-10")
+periodReturn(AAPL,period="daily",type="arithmetic")
+periodReturn(AAPL,period="daily",type="log")
+dailyReturn(AAPL)
+##全收益率
+allReturns(AAPL)
+
+####画图
+chartSeries(AAPL)
+chartSeries(AAPL,theme="white")
+chartSeries(AAPL,name="AAPL CHART",subset="2013-10-01::2013-10-23")
+chartSeries(AAPL,name="AAPL CHART",type="bars")
+chartSeries(AAPL,name="AAPL CHART",type="line")
+chartSeries(AAPL,name="AAPL CHART",type="candlesticks")
+
+##barChart()   ##同chartSeries(AAPL,type="bars")
+barChart(AAPL)  
+barChart(AAPL,theme="white",subset="2012-12::2013")
+##用last/first作时间段
+barChart(AAPL,subset="last 10 weeks")
+barChart(AAPL,subset="first 10 weeks")
+barChart(AAPL,subset="first 10 weeks",bar.type="ohlc")
+barChart(AAPL,subset="first 10 weeks",bar.type="hlc")
+
+##candleChart()   ##同chartSeries(AAPL)
+candleChart(AAPL,theme="white",subset="2013-10-05::2013-10-30")
+
+
+
+##在已画过的图上修改
+reChart(type="bars",subset="2013-10-05::2013-10-29", show.grid=TRUE)
+
 
